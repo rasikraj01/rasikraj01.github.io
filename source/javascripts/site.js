6739cb25821ee2cfa61c502cd247aa6c1d51ae29
Rasik <rasikraj01@gmail.com>
2018-08-05 23:12:03 +0530

    project transition added

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,6 +2,8 @@ const projects = document.getElementById('projects');
 const sidebar = document.querySelectorAll('.sidebar');
 const social_media = document.querySelectorAll('.social-media');
 const view_1 = document.querySelectorAll('.view-1');
+const view_2 = document.querySelectorAll('.view-2');
+const topbar = document.querySelectorAll('.topbar');
 
 projects.addEventListener('click', () => {
 
@@ -15,4 +17,10 @@ projects.addEventListener('click', () => {
       social_media[0].style.display = 'none';
       view_1[0].style.display = 'none';
    }, 500);
+   view_2[0].style.display = 'flex';
+   topbar[0].style.display = 'block';
+   setTimeout(() => {
+      view_2[0].classList.add('view-2-on');
+      topbar[0].classList.add('topbar-on');
+   }, 700);
 })
